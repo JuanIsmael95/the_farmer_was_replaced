a3be85691d964a4c3d0c9a004daba7ad41bb91f7
JuanIsmael95 <jismaellp95@gmail.com>
2026-06-26 23:11:20 +0100

    Update Calabazas.py

--- Calabazas.py
+++ Calabazas.py
@@ -1,8 +1,17 @@
+'''
+Here I try to maximize the value of pumpkins before being able to use more than one drone.
+That's why I set the world size to 8, so i can have the maximum value of the 8x8 pumpkin without it being
+too slow
+'''
+
 set_world_size(8)
 clear()
 
+#I don't set size=8 in case I use this code for a bigger farm
 size = get_world_size()
-
+'''
+This function only plants pumpkins in all the grid, nothing really complicated here.
+'''
 def plantar_calabazas():
 	for i in range(size):
 		for j in range(size):
@@ -11,7 +20,11 @@ def plantar_calabazas():
 			plant(Entities.Pumpkin)
 			move(East)
 		move(North)
-
+'''
+This function is made to check if there is any dead pumpkin in the farm and the 8x8 pumpkin can't be made.
+it starts with a boolean value set to false, and the loop only ends if the drone checks all pumpkins and there isn't
+any dead pumpkin, in case a replanted pumpkin becomes again in a dead pumpkin.
+'''
 def se_puede_cosechar():
 	mal = False
 	while(mal == False):
@@ -24,7 +37,12 @@ def se_puede_cosechar():
 						mal = False
 				move(East)
 			move(North)
-				
+
+'''
+This function only exists because in early versions when the farm was smaller I had to wait before checking for dead pumpkins, 
+because usually when the check began, the pumpkin in (0,0) wasn't grown yet, the drone saw nothing wrong and later it would grow into a dead one.
+But when the farm is bigger it isn't needed.
+'''
 def esperar():
 	for i in range(2):
 		pet_the_piggy()
@@ -32,7 +50,9 @@ def esperar():
 
 
 	
-	
+'''
+Nothing to say here I think :$
+'''
 def main():
 	plantar_calabazas()
 	se_puede_cosechar()
